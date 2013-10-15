df6a83ac1f6f12b8e00e692674e89060ae6f7894
Ryan Vergeront <ryanvergeront@gmail.com>
2013-10-15 14:47:58 -0400

    Update namespacing module with new filepaths

--- lib/bit_torrent_client.rb
+++ lib/bit_torrent_client.rb
@@ -1,3 +1,8 @@
-class BittorrentClient
+require_relative "bit_torrent_client/metainfo"
+require_relative "bit_torrent_client/metainfo_parser"
+require_relative "bit_torrent_client/info_dictionary"
+require_relative "bit_torrent_client/piece"
+require_relative "bit_torrent_client/downloadable_file"
 
+module BitTorrentClient
 end
