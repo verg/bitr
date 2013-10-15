b4aeba90f32f950193ca1f291fbad99387beed7c
Ryan Vergeront <ryanvergeront@gmail.com>
2013-10-15 14:43:18 -0400

    Adds MetainfoParser to wrap bencode gem

--- lib/bit_torrent_client/metainfo_parser.rb
+++ lib/bit_torrent_client/metainfo_parser.rb
@@ -4,6 +4,7 @@ module BitTorrentClient
   class MetainfoParser
     def self.parse(torrent)
       metainfo_hash = BEncode::Parser.new(torrent).parse!
+      metainfo_hash['info']["pieces"] = metainfo_hash['info']['pieces'].encode('UTF-8', 'binary', invalid: :replace, undef: :replace)
     end
   end
 end
--- spec/lib/bit_torrent_client/metainfo_parser_spec.rb
+++ spec/lib/bit_torrent_client/metainfo_parser_spec.rb
@@ -3,10 +3,15 @@ require_relative "../../../lib/bit_torrent_client/metainfo"
 
 module BitTorrentClient
   describe MetainfoParser do
-    let(:torrent) { File.read("spec/fixtures/flagfromserver.torrent") }
+    let(:single_file_torrent) { File.read("spec/fixtures/flagfromserver.torrent") }
+    let(:multifile_torrent) { File.read("spec/fixtures/python.torrent") }
 
-    it "sends the torrent file to the parser" do
-      MetainfoParser.parse(torrent)
+    it "parses single file torrents" do
+      MetainfoParser.parse(single_file_torrent)
+    end
+
+    it "parses multifile torrents" do
+      MetainfoParser.parse(multifile_torrent)
     end
   end
 end
