57645d7904fc39f92584f4b079cfec15ab2d4bb6
Ryan Vergeront <ryanvergeront@gmail.com>
2013-10-15 19:08:47 -0400

    Adds a info hash to Metainfo

--- lib/bit_torrent_client/metainfo.rb
+++ lib/bit_torrent_client/metainfo.rb
@@ -1,13 +1,24 @@
+require 'bencode'
+
 module BitTorrentClient
   class Metainfo
-    attr_reader :announce, :creation_date, :created_by, :encoding
+    attr_reader :announce, :creation_date, :created_by, :encoding, :info_hash
 
     def initialize(args)
       @announce = args.fetch("announce")
       @created_by = args["created by"]
       @creation_date = args["creation date"]
       @encoding = args["encoding"]
-      @info_dictionary = InfoDictionary.new(args['info'])
+      @info_dictionary = args['info_dictionary'] || InfoDictionary.new(args['info'])
+      @info_hash = create_info_hash(args['info'])
+    end
+
+    def pieces
+      @info_dictionary.pieces
+    end
+
+    def create_info_hash(info_hash)
+      Digest::SHA1.new.digest(info_hash.bencode)
     end
   end
 end
--- spec/lib/bit_torrent_client/metainfo_spec.rb
+++ spec/lib/bit_torrent_client/metainfo_spec.rb
@@ -42,8 +42,21 @@ module BitTorrentClient
       InfoDictionary.should_receive(:new).with( {"length"=>1277987,
                                                 "name"=>"flag.jpg",
                                                 "piece length"=>16384,
-                                                "pieces"=> pieces })
+                                                "pieces"=> pieces } )
       Metainfo.new(metainfo_hash)
     end
+
+    it "stores an info hash as a SHA1 of the value of the bencoded info key" do
+      metainfo = Metainfo.new(metainfo_hash)
+      expect(metainfo.info_hash.size).to eq 20
+    end
+
+    it "has pieces through it's info dictionary" do
+      metainfo = Metainfo.new(metainfo_hash)
+      expect(metainfo.pieces.size).to eq 1
+    end
+
+    it "knows the amount of bytes uploaded"
+    it "knows the amount of bytes downloaded"
   end
 end
