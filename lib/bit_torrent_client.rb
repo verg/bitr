8eb1940413a45f7113ef99546b3955e83500744f
Ryan Vergeront <ryanvergeront@gmail.com>
2013-10-16 14:51:59 -0400

    Adds Peer

--- lib/bit_torrent_client.rb
+++ lib/bit_torrent_client.rb
@@ -3,6 +3,7 @@ require_relative "bit_torrent_client/http_client"
 require_relative "bit_torrent_client/metainfo_parser"
 require_relative "bit_torrent_client/info_dictionary"
 require_relative "bit_torrent_client/piece"
+require_relative "bit_torrent_client/peer"
 require_relative "bit_torrent_client/downloadable_file"
 require_relative "bit_torrent_client/announce_response"
 
---
+++ lib/bit_torrent_client/peer.rb
@@ -0,0 +1,14 @@
+class Peer
+  attr_reader :ip, :port
+  attr_accessor :choking, :interested
+  alias_method :choking?, :choking
+  alias_method :interested?, :interested
+
+  def initialize(args)
+    @ip = args.fetch(:ip)
+    @port = args.fetch(:port)
+    @choking = true
+    @interested = false
+  end
+
+end
