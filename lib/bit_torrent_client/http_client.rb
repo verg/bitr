5973e97fd0219c8be29aa40673ef71a4927de52a
Ryan Vergeront <ryanvergeront@gmail.com>
2013-10-16 10:27:07 -0400

    Refactor HTTPClient

--- lib/bit_torrent_client/http_client.rb
+++ lib/bit_torrent_client/http_client.rb
@@ -9,24 +9,30 @@ module BitTorrentClient
     end
 
     def get_start_event(metainfo)
+      request_params = build_request_params(metainfo)
+
+      uri = URI(metainfo.announce)
+      uri.query = URI.encode_www_form(request_params)
+      response = Net::HTTP.get_response(uri)
+    end
+
+    private
+
+    def build_request_params(metainfo)
       if @compact
         compact_param = { 'compact' => 1 }
       else
         compact_param = { 'compact' => 0 }
       end
 
-      request_params = { 'info_hash' => metainfo.info_hash,
-                         'peer_id' => @peer_id,
-                         'port' => @port,
-                         'uploaded' => metainfo.uploaded_bytes,
-                         'downloaded' => metainfo.downloaded_bytes,
-                         'left' => metainfo.bytes_left,
-                         'no_peer_id' => 0,
-                         'event' => "started" }.merge(compact_param)
-
-      uri = URI(metainfo.announce)
-      uri.query = URI.encode_www_form(request_params)
-      response = Net::HTTP.get_response(uri)
+      { 'info_hash' => metainfo.info_hash,
+        'peer_id' => @peer_id,
+        'port' => @port,
+        'uploaded' => metainfo.uploaded_bytes,
+        'downloaded' => metainfo.downloaded_bytes,
+        'left' => metainfo.bytes_left,
+        'no_peer_id' => 0,
+        'event' => "started" }.merge(compact_param)
     end
   end
 end
