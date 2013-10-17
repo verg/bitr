da7c04e515fd917e4890eeadbaedb8ea100cb73c
Ryan Vergeront <ryanvergeront@gmail.com>
2013-10-17 16:01:54 -0400

    Fix bad info hash parse bug

--- lib/bit_torrent_client/metainfo_parser.rb
+++ lib/bit_torrent_client/metainfo_parser.rb
@@ -3,8 +3,7 @@ require 'bencode'
 module BitTorrentClient
   class MetainfoParser
     def self.parse(torrent)
-       metainfo_hash = BEncode::Parser.new(torrent).parse!
-      metainfo_hash['info']["pieces"] = metainfo_hash['info']['pieces'].encode('UTF-8', 'binary', invalid: :replace, undef: :replace)
+      metainfo_hash = BEncode::Parser.new(torrent).parse!
       metainfo_hash
     end
   end
