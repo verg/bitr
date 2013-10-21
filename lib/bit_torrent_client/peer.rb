0d44d9743c7989dcf505daf446f0a846b71a8344
Ryan Vergeront <ryanvergeront@gmail.com>
2013-10-21 17:33:39 -0400

    Add missing module namespacing to Peer & spec

--- lib/bit_torrent_client/peer.rb
+++ lib/bit_torrent_client/peer.rb
@@ -1,14 +1,15 @@
-class Peer
-  attr_reader :ip, :port
-  attr_accessor :choking, :interested
-  alias_method :choking?, :choking
-  alias_method :interested?, :interested
+module BitTorrentClient
+  class Peer
+    attr_reader :ip, :port
+    attr_accessor :choking, :interested
+    alias_method :choking?, :choking
+    alias_method :interested?, :interested
 
-  def initialize(args)
-    @ip = args.fetch(:ip)
-    @port = args.fetch(:port)
-    @choking = true
-    @interested = false
+    def initialize(args)
+      @ip = args.fetch(:ip)
+      @port = args.fetch(:port)
+      @choking = true
+      @interested = false
+    end
   end
-
 end
--- spec/lib/bit_torrent_client/peer_spec.rb
+++ spec/lib/bit_torrent_client/peer_spec.rb
@@ -1,25 +1,27 @@
 require_relative "../../../lib/bit_torrent_client/peer"
 
-describe Peer do
-  let(:peer) { Peer.new({:ip=>"74.212.183.186", :port=>6881}) }
+module BitTorrentClient
+  describe Peer do
+    let(:peer) { Peer.new({:ip=>"74.212.183.186", :port=>6881}) }
 
-  it "has a choking status that can be set" do
-    expect(peer.choking?).to be true
-    peer.choking = false
-    expect(peer.choking?).to be false
-  end
+    it "has a choking status that can be set" do
+      expect(peer.choking?).to be true
+      peer.choking = false
+      expect(peer.choking?).to be false
+    end
 
-  it "has a interested status" do
-    expect(peer.interested?).to be false
-    peer.interested = true
-    expect(peer.interested?).to be true
-  end
+    it "has a interested status" do
+      expect(peer.interested?).to be false
+      peer.interested = true
+      expect(peer.interested?).to be true
+    end
 
-  it "has an ip address" do
-    expect(peer.ip).to eq '74.212.183.186'
-  end
+    it "has an ip address" do
+      expect(peer.ip).to eq '74.212.183.186'
+    end
 
-  it "has a port" do
-    expect(peer.port).to eq 6881
+    it "has a port" do
+      expect(peer.port).to eq 6881
+    end
   end
 end
