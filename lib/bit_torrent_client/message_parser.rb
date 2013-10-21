21e37b105d486485184fce763cc977ca41b77f7b
Ryan Vergeront <ryanvergeront@gmail.com>
2013-10-21 17:34:07 -0400

    Add missing module namespacing to MessageParser

--- lib/bit_torrent_client/message_parser.rb
+++ lib/bit_torrent_client/message_parser.rb
@@ -1,21 +1,23 @@
-class MessageParser
-  attr_reader :length, :type
+module BitTorrentClient
+  class MessageParser
+    attr_reader :length, :type
 
-  MESSAGE_TYPES  = { 5 => :bitfield }
+    MESSAGE_TYPES  = { 5 => :bitfield }
 
-  def initialize(message)
-    @message = message
-    @length = @message[0..3].unpack("N").first
-  end
+    def initialize(message)
+      @message = message
+      @length = @message[0..3].unpack("N").first
+    end
 
-  def get_type
-    @message
-  end
+    def get_type
+      @message
+    end
 
-  def type
-    return :keep_alive if @length == 0
+    def type
+      return :keep_alive if @length == 0
 
-    id = @message[4].unpack("C").first
-    MESSAGE_TYPES[id]
+      id = @message[4].unpack("C").first
+      MESSAGE_TYPES[id]
+    end
   end
 end
--- spec/lib/bit_torrent_client/message_parser_spec.rb
+++ spec/lib/bit_torrent_client/message_parser_spec.rb
@@ -1,18 +1,20 @@
 require_relative "../../../lib/bit_torrent_client/message_parser"
 
-describe MessageParser do
-  describe "bitfield messages" do
-    let(:bitfield) { "\x00\x00\x00\v\x05\xFF\xFB\xFF\xFF\xFF\xEE\x7F\xFD\xBF\xEE" }
-    let(:size_of_length_prefix) { 4 }
+module BitTorrentClient
+  describe MessageParser do
+    describe "bitfield messages" do
+      let(:bitfield) { "\x00\x00\x00\v\x05\xFF\xFB\xFF\xFF\xFF\xEE\x7F\xFD\xBF\xEE" }
+      let(:size_of_length_prefix) { 4 }
 
-    it "finds the messages length" do
-      parser = MessageParser.new(bitfield)
-      expect(parser.length).to eq bitfield.size - size_of_length_prefix
-    end
+      it "finds the messages length" do
+        parser = MessageParser.new(bitfield)
+        expect(parser.length).to eq bitfield.size - size_of_length_prefix
+      end
 
-    it "identifies when a message is a handshake" do
-      parser = MessageParser.new(bitfield)
-      expect(parser.type).to eq :bitfield
+      it "identifies when a message is a handshake" do
+        parser = MessageParser.new(bitfield)
+        expect(parser.type).to eq :bitfield
+      end
     end
   end
 end
