102e2e9ff7a5eaffb05e8d4622238929e79ac373
Ryan Vergeront <ryanvergeront@gmail.com>
2013-10-21 17:34:32 -0400

    Add missing module namespaceing to TCPClient

--- lib/bit_torrent_client/tcp_client.rb
+++ lib/bit_torrent_client/tcp_client.rb
@@ -1,33 +1,34 @@
-class TCPClient
+module BitTorrentClient
+  class TCPClient
 
-  SIXTEEN_KB = 1024 * 16
+    SIXTEEN_KB = 1024 * 16
 
-  def initialize(peer, client_id, info_hash)
-    @peer = peer
-    @client_id = client_id
-    @info_hash = info_hash
-  end
-
-  def send_handshake
-    socket = TCPSocket.new(@peer.ip, @peer.port)
-    socket.write handshake_message
-    # byte = socket.getbyte
-    begin
-      data = socket.read_nonblock(SIXTEEN_KB)
-    rescue Errno::EAGAIN
-      retry
+    def initialize(peer, client_id, info_hash)
+      @peer = peer
+      @client_id = client_id
+      @info_hash = info_hash
     end
 
-    data
-  end
+    def send_handshake
+      socket = TCPSocket.new(@peer.ip, @peer.port)
+      socket.write handshake_message
+      # byte = socket.getbyte
+      begin
+        data = socket.read_nonblock(SIXTEEN_KB)
+      rescue Errno::EAGAIN
+        retry
+      end
+      data
+    end
 
-  def handshake_message
-    {
-      pstrlen:    "\x13",
-      pstr:       "BitTorrent protocol",
-      reserved:   "\x00\x00\x00\x00\x00\x00\x00\x00",
-      info_hash:  "#{@info_hash}",
-      peer_id:    "#{@client_id}"
-    }.values.join("")
+    def handshake_message
+      {
+        pstrlen:    "\x13",
+        pstr:       "BitTorrent protocol",
+        reserved:   "\x00\x00\x00\x00\x00\x00\x00\x00",
+        info_hash:  "#{@info_hash}",
+        peer_id:    "#{@client_id}"
+      }.values.join("")
+    end
   end
 end
--- spec/lib/bit_torrent_client/tcp_client_spec.rb
+++ spec/lib/bit_torrent_client/tcp_client_spec.rb
@@ -1,21 +1,23 @@
 require_relative "../../../lib/bit_torrent_client"
 
-describe TCPClient do
+module BitTorrentClient
+  describe TCPClient do
 
-  #let(:info_hash) { "%2b%15%ca%2b%fdH%cd%d7m9%ecU%a3%ab%1b%8aW%18%0a%09" }
-  #let(:peer) { Peer.new({:ip=>"74.212.183.186", :port=>6881}) }
-  #torrent_file = File.read("spec/fixtures/flagfromserver.torrent")
-  #info_hash = Metainfo.new(MetainfoParser.parse(torrent_file)).info_hash
-  #let(:tcp_client) { TCPClient.new(peer, "-RV0001-000000000002", info_hash) }
-  xit "has a handshake" do
-    torrent_file = File.read("spec/fixtures/flagfromserver.torrent")
-    message = tcp_client.handshake_message
-    expect(message).to eq "\\x13BitTorrent protocol\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00%E5%8F%CA%C55%AB%13H-%96%C1W%FA1%1A3/k%FA%86-RV0001-000000000002"
+    #let(:info_hash) { "%2b%15%ca%2b%fdH%cd%d7m9%ecU%a3%ab%1b%8aW%18%0a%09" }
+    #let(:peer) { Peer.new({:ip=>"74.212.183.186", :port=>6881}) }
+    #torrent_file = File.read("spec/fixtures/flagfromserver.torrent")
+    #info_hash = Metainfo.new(MetainfoParser.parse(torrent_file)).info_hash
+    #let(:tcp_client) { TCPClient.new(peer, "-RV0001-000000000002", info_hash) }
+    xit "has a handshake" do
+      torrent_file = File.read("spec/fixtures/flagfromserver.torrent")
+      message = tcp_client.handshake_message
+      expect(message).to eq "\\x13BitTorrent protocol\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00%E5%8F%CA%C55%AB%13H-%96%C1W%FA1%1A3/k%FA%86-RV0001-000000000002"
+    end
+
+    xit "shakes hands with the client" do
+      response = tcp_client.send_handshake
+      expect(response).to eq ''
+    end
+
   end
-
-  xit "shakes hands with the client" do
-    response = tcp_client.send_handshake
-    expect(response).to eq ''
-  end
-
 end
