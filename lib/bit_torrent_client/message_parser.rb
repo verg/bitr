c2137879d23c68807b8bdb908a139716c64c95e8
Ryan Vergeront <ryanvergeront@gmail.com>
2013-10-21 15:13:30 -0400

    Start implementing MessageParser

---
+++ lib/bit_torrent_client/message_parser.rb
@@ -0,0 +1,21 @@
+class MessageParser
+  attr_reader :length, :type
+
+  MESSAGE_TYPES  = { 5 => :bitfield }
+
+  def initialize(message)
+    @message = message
+    @length = @message[0..3].unpack("C*").join("").to_i
+  end
+
+  def get_type
+    @message
+  end
+
+  def type
+    return :keep_alive if @length == 0
+
+    id = @message[4].unpack("C").first
+    MESSAGE_TYPES[id]
+  end
+end
