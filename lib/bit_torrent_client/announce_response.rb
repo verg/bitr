4f807b1fca6c76e9f444c8464143e23aa19c2be6
Ryan Vergeront <ryanvergeront@gmail.com>
2013-10-21 17:32:53 -0400

    Add missing namespace to AnnounceResponse & spec

--- lib/bit_torrent_client/announce_response.rb
+++ lib/bit_torrent_client/announce_response.rb
@@ -1,31 +1,33 @@
 require 'bencode'
 require 'ipaddr'
 
-class AnnounceResponse
-  attr_reader :response_body, :seeders, :leachers, :interval, :peers
+module BitTorrentClient
+  class AnnounceResponse
+    attr_reader :response_body, :seeders, :leachers, :interval, :peers
 
-  def initialize(response_body)
-    @response_body = response_body
-    @seeders = parsed_response_body.fetch("complete")
-    @leachers = parsed_response_body.fetch("incomplete")
-    @interval = parsed_response_body.fetch("interval")
-    @peers = get_peers
-  end
-
-  private
-
-  def parsed_response_body
-    @parsed ||= BEncode.load(@response_body)
-  end
-
-  def get_peers
-    decode_peers.map! do |peer|
-      ip_bytes, port = peer.unpack("a4n")
-      { ip: IPAddr.new_ntoh(ip_bytes).to_s, port: port }
+    def initialize(response_body)
+      @response_body = response_body
+      @seeders = parsed_response_body.fetch("complete")
+      @leachers = parsed_response_body.fetch("incomplete")
+      @interval = parsed_response_body.fetch("interval")
+      @peers = get_peers
     end
-  end
 
-  def decode_peers
-    BEncode.load(@response_body)['peers'].scan(/.{6}/)
+    private
+
+    def parsed_response_body
+      @parsed ||= BEncode.load(@response_body)
+    end
+
+    def get_peers
+      decode_peers.map! do |peer|
+        ip_bytes, port = peer.unpack("a4n")
+        { ip: IPAddr.new_ntoh(ip_bytes).to_s, port: port }
+      end
+    end
+
+    def decode_peers
+      BEncode.load(@response_body)['peers'].scan(/.{6}/)
+    end
   end
 end
