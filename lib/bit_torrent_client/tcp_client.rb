d9e72b10ba830e4d3811d6c11cbf2515add8ddab
Adam Pash <adam.pash@gmail.com>
2013-10-22 15:52:46 -0400

    Sending interested message to peer

--- lib/bit_torrent_client/tcp_client.rb
+++ lib/bit_torrent_client/tcp_client.rb
@@ -7,23 +7,41 @@ module BitTorrentClient
       @peer = peer
       @client_id = client_id
       @info_hash = info_hash
+      @socket = TCPSocket.new(@peer.ip, @peer.port)
     end
 
-    def send_handshake
-      socket = TCPSocket.new(@peer.ip, @peer.port)
-      socket.write handshake_message
+    def exchange_handshake
+      @socket.write handshake_message
       # byte = socket.getbyte
       begin
-        data = socket.read_nonblock(SIXTEEN_KB)
+        data = @socket.read_nonblock(SIXTEEN_KB)
       rescue Errno::EAGAIN
         retry
       end
       data
     end
 
+    def declare_interest
+      @socket.write interested_message
+      begin
+        data = @socket.read_nonblock(SIXTEEN_KB)
+      rescue Errno::EAGAIN
+        retry
+      end
+      data
+    end
+
+    def shutdown
+      @socket.close if @socket
+    end
+
     def handshake_message
       MessageBuilder.build(:handshake, info_hash: @info_hash,
                            client_id: @client_id).to_s
+    end
+
+    def interested_message
+      MessageBuilder.build(:interested).to_s
     end
   end
 end
--- spec/integration/network_spec.rb
+++ spec/integration/network_spec.rb
@@ -5,6 +5,7 @@ require 'vcr'
 
 module BitTorrentClient
   describe 'Network' do
+
     it "shakes hands with a peer" do
       torrent_file = File.read("spec/fixtures/flagfromserver.torrent")
       metainfo = Metainfo.new(MetainfoParser.parse(torrent_file))
@@ -19,8 +20,31 @@ module BitTorrentClient
       peers.map! { |peer| Peer.new(peer) }
       THE_RIGHT_PEER = peers.select { |peer| peer.ip == "96.126.104.219" }.first
       socket = TCPClient.new(THE_RIGHT_PEER, peer_id, metainfo.info_hash)
-      data = socket.send_handshake
+      data = socket.exchange_handshake
       expect(handle_encoding(data)).to match(/BitTorrent protocol/)
+      socket.shutdown
+    end
+
+    it "sends an interested message and receives an unchoke response" do
+      torrent_file = File.read("spec/fixtures/flagfromserver.torrent")
+      metainfo = Metainfo.new(MetainfoParser.parse(torrent_file))
+      peer_id = "-RV0001-000000000002"
+
+      metainfo.stub(:uploaded_bytes) { 0 }
+      metainfo.stub(:downloaded_bytes) { 0 }
+      metainfo.stub(:bytes_left) { 16384 }
+
+      response = HTTPClient.new(peer_id).get_start_event(metainfo)
+      peers = response.peers
+      peers.map! { |peer| Peer.new(peer) }
+      THE_RIGHT_PEER = peers.select { |peer| peer.ip == "96.126.104.219" }.first
+      socket = TCPClient.new(THE_RIGHT_PEER, peer_id, metainfo.info_hash)
+      socket.exchange_handshake
+      data = socket.declare_interest
+      messages = MessageHandler.new.handle(data)
+      expect(messages.first.type).to eq :unchoke
+
+      socket.shutdown
     end
 
     def handle_encoding(string)
