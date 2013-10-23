b7f104687695ef7256f74c2dd73519e88204d176
Adam Pash <adam.pash@gmail.com>
2013-10-23 15:36:44 -0400

    MetainfoParser can parse string or file

--- lib/bit_torrent_client/metainfo_parser.rb
+++ lib/bit_torrent_client/metainfo_parser.rb
@@ -3,6 +3,11 @@ require 'bencode'
 module BitTorrentClient
   class MetainfoParser
     def self.parse(torrent)
+      begin
+        torrent = File.read(torrent)
+      rescue ArgumentError
+        torrent = torrent
+      end
       metainfo_hash = BEncode::Parser.new(torrent).parse!
       metainfo_hash
     end
--- spec/lib/bit_torrent_client/metainfo_parser_spec.rb
+++ spec/lib/bit_torrent_client/metainfo_parser_spec.rb
@@ -4,17 +4,23 @@ require_relative "../../../lib/bit_torrent_client/metainfo"
 module BitTorrentClient
   describe MetainfoParser do
     let(:single_file_torrent) { File.read("spec/fixtures/flagfromserver.torrent") }
+    let(:torrent_file_string) { "spec/fixtures/flagfromserver.torrent" }
     let(:multifile_torrent) { File.read("spec/fixtures/python.torrent") }
 
     it "parses single file torrents" do
-      MetainfoParser.parse(single_file_torrent)
+      parsed_file = MetainfoParser.parse(single_file_torrent)
+      expect(parsed_file['announce']).to eq 'http://thomasballinger.com:6969/announce'
     end
 
     it "parses multifile torrents" do
-      MetainfoParser.parse(multifile_torrent)
+      parsed_file = MetainfoParser.parse(multifile_torrent)
+      expect(parsed_file['announce']).to eq 'http://tracker001.legaltorrents.com:7070/announce'
     end
 
-    it "can parse a torrent string"
+    it "can parse a torrent string" do
+      parsed_file = MetainfoParser.parse(torrent_file_string)
+      expect(parsed_file['announce']).to eq 'http://thomasballinger.com:6969/announce'
+    end
     it "can parse a torrent file"
   end
 end
