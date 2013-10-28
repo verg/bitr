e4a20cb7b6c69a5c7dd175f36a4908bf1872d6fd
Adam Pash <adam.pash@gmail.com>
2013-10-28 15:07:10 -0400

    Tracking what pieces a peer has by the bitfield

--- lib/bit_torrent_client/peer.rb
+++ lib/bit_torrent_client/peer.rb
@@ -10,6 +10,26 @@ module BitTorrentClient
       @port = args.fetch(:port)
       @choking = true
       @interested = false
+      @available_pieces = []
+    end
+
+    def process_bitfield(bitfield)
+      @available_pieces = unpack_bitfield(bitfield).map do |char|
+        char == "1" ? true : false
+      end
+    end
+
+    def has_piece?(index)
+      @available_pieces[index]
+    end
+
+    def has_piece_at(index)
+      @available_pieces[index] = true
+    end
+
+    private
+    def unpack_bitfield(bitfield)
+      bitfield.unpack("B*").first.chars
     end
   end
 end
--- spec/lib/bit_torrent_client/peer_spec.rb
+++ spec/lib/bit_torrent_client/peer_spec.rb
@@ -3,6 +3,7 @@ require_relative "../../../lib/bit_torrent_client/peer"
 module BitTorrentClient
   describe Peer do
     let(:peer) { Peer.new({:ip=>"74.212.183.186", :port=>6881}) }
+    let(:bitfield) { "\xff\xff\x2f\xf6\xff\xff\xff\xf7\xdf\xfe" }
 
     it "has a choking status that can be set" do
       expect(peer.choking?).to be true
@@ -23,5 +24,21 @@ module BitTorrentClient
     it "has a port" do
       expect(peer.port).to eq 6881
     end
+
+    it "translates a bitfield into pieces it has or does not have" do
+      has_piece_index = 0
+      no_piece_index = 16
+      peer.process_bitfield(bitfield)
+      expect(peer.has_piece?(has_piece_index)).to be true
+      expect(peer.has_piece?(no_piece_index)).to be false
+    end
+
+    it "updates the pieces it has by that piece index" do
+      piece_index = 16
+      peer.process_bitfield(bitfield)
+      peer.has_piece_at(piece_index)
+      expect(peer.has_piece?(piece_index)).to eq true
+    end
+
   end
 end
