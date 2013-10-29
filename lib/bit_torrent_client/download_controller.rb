4efa4c24841cf4fed15eb8c3a6497ac2427f85c8
Adam Pash <adam.pash@gmail.com>
2013-10-29 16:29:49 -0400

    Testing DownloadController

--- lib/bit_torrent_client/download_controller.rb
+++ lib/bit_torrent_client/download_controller.rb
@@ -1,7 +1,39 @@
 module BitTorrentClient
   class DownloadController
+    attr_reader :sockets, :pieces
+    def initialize(pieces)
+      @sockets = []
+      @pieces = pieces
+    end
+
+    def add_socket(socket)
+      @sockets << socket
+
+      tick
+    end
+
+    def tick
+
+    end
+
+    def send_request(socket)
+      piece = next_piece
+      socket.request_piece(piece.index,
+                           piece.next_byte_offset,
+                           BitTorrentClient.hex_block_bytes)
+    end
+
+    def remove_socket(socket)
+      @sockets.delete(socket)
+    end
+
     def pending_requests
       0
     end
+
+    def next_piece
+      pieces.incomplete.first
+    end
+
   end
 end
--- spec/lib/bit_torrent_client/download_controller_spec.rb
+++ spec/lib/bit_torrent_client/download_controller_spec.rb
@@ -1,9 +1,22 @@
 require_relative "../../../lib/bit_torrent_client/download_controller"
+require_relative "../../../lib/bit_torrent_client/piece_collection"
+require_relative "../../../lib/bit_torrent_client/config"
 
 module BitTorrentClient
   describe DownloadController do
     let(:socket) { double("tcp connection") }
-    let(:controller) { DownloadController.new() }
+    let(:socket_2) { double("tcp connection") }
+    let(:piece) {
+      double("piece",
+             incomplete_blocks: [:block1, :block2],
+             :complete? => false,
+             :index => "\x00\x00\x00\x00",
+             :next_byte_offset => "\x00\x00\x00\x00")
+    }
+    let(:piece_1) { double("piece one", :complete? => false) }
+    let(:piece_2) { double("piece two", :complete? => false) }
+    let(:pieces) { PieceCollection.new([piece, piece_1, piece_2]) }
+    let(:controller) { DownloadController.new(pieces) }
 
     describe "tracking outstaning requests" do
       it "starts with no connections" do
@@ -13,20 +26,65 @@ module BitTorrentClient
       it "decrements pending requests when a piece is finsished"
     end
 
-    it "has a socket" do
-      expect(controller.socket).to
+    it "knows if it has any sockets" do
+      expect(controller.has_sockets?).to be false
+      controller.add_socket(socket)
+      expect(controller.has_sockets?).to be true
     end
-    it "holds onto a peers collection"
-    it "holds onto the status of pieces"
 
-    it "starts requesting pieces when it has at least one peer"
+    it "knows if it needs pieces"
 
-    #  socket: conn, peice: current stata, peer: haves
+    it "knows if it's maxed out its connections"
 
-    it "can recieve a new socket"
-    it "can remove a socket"
+    it "knows if it can make another request"
 
-    it "requests pieces we need"
+    describe "#tick" do
+      it "sends a request if has sockets, needs pieces, and hasn't maxed connections" do
+        controller.should_receive(:tick)
+        controller.add_socket(socket)
+      end
+    end
+
+    describe "sockets" do
+      it "has an array of sockets" do
+        expect(controller.sockets.length).to eq 0
+      end
+      it "can recieve a new socket" do
+        socket.stub(:request_piece)
+        controller.add_socket(socket)
+        expect(controller.sockets.length).to eq 1
+      end
+
+      it "can remove a socket" do
+        socket.stub(:request_piece)
+        socket_2.stub(:request_piece)
+        controller.add_socket(socket)
+        controller.add_socket(socket_2)
+        controller.remove_socket(socket)
+        expect(controller.sockets.length).to eq 1
+      end
+    end
+
+    describe "tracking pieces" do
+
+      it "holds onto the status of pieces" do
+        expect(controller.pieces.length).to eq 3
+      end
+
+      xit "starts requesting pieces when it has at least one socket" do
+        socket.should_receive(:tick)
+        controller.add_socket(socket)
+      end
+
+      it "determines the next piece we want" do
+        expect(controller.next_piece).to eq piece
+      end
+
+      #  socket: conn, peice: current stata, peer: haves
+
+      it "requests pieces we need"
+
+    end
     it "makes requests upto the max request number"
     it "requests missing blocks"
     it "makes new requests when other pieces are completed"
