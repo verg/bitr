4a75843feab884b8bb3e85d50e7d5262dabe9ef8
Adam Pash <adam.pash@gmail.com>
2013-11-05 12:15:36 -0500

    FileReader reads bytes

---
+++ lib/bit_torrent_client/file_reader.rb
@@ -0,0 +1,27 @@
+module BitTorrentClient
+  class FileReader
+
+    attr_reader :files
+    def initialize(info_dictionary)
+      @info_dictionary = info_dictionary
+      @files = @info_dictionary.files
+    end
+
+    def files_to_read(byte_range)
+      @files.select { |file| file.byte_overlap?(byte_range) }
+    end
+
+    def read(byte_range)
+      bytes = ''
+      files_to_read(byte_range).each do |file|
+        seek_point = file.find_seek_point(byte_range.begin)
+        bytes_to_read = byte_range.end - file.start_offset
+        File.open(file.full_path, "r") do |f|
+          f.seek(seek_point)
+          bytes << f.read(bytes_to_read )
+        end
+      end
+      bytes
+    end
+  end
+end
---
+++ spec/lib/bit_torrent_client/file_reader_spec.rb
@@ -0,0 +1,76 @@
+require_relative "../../../lib/bit_torrent_client/file_reader"
+require_relative "../../../lib/bit_torrent_client/downloadable_file"
+
+module BitTorrentClient
+  describe FileReader do
+    let(:first_file) { {"filename" => "first_file", "byte_size" => 4,
+                        "start_offset" => 0} }
+    let(:other_file) { { "filename" => "other_file", "byte_size" => 5,
+                         "start_offset" => 4} }
+
+    it "knows of some files" do
+      file = DownloadableFile.new(first_file)
+      info_dictionary = double("info_dictionary", files: [file].flatten)
+      reader = FileReader.new(info_dictionary)
+      expect(reader.files).to eq [file]
+    end
+
+
+    describe "reads specific bytes within a file" do
+      it "identifies files that contain the specified bytes" do
+        byte_range = 8..9
+        file_with_bytes = double("file", byte_overlap?: true)
+        file_outside_of_bytes = double("file", byte_overlap?: false)
+        info_dictionary = double("info_dictionary",
+                                 files: [file_with_bytes, file_outside_of_bytes])
+
+        reader = FileReader.new(info_dictionary)
+        expect(reader.files_to_read(byte_range)).to eq [file_with_bytes]
+      end
+
+      context "when reading from a single file" do
+        it "reads data from the correct range" do
+          File.open(first_file['filename'], "w") do |f|
+            f.write("\x00\x01\x02\x03")
+          end
+
+          file = DownloadableFile.new(first_file)
+          info_dictionary = double("info_dictionary", files: [file].flatten)
+          reader = FileReader.new(info_dictionary)
+
+          byte_range = 0...4
+          read_data = reader.read(byte_range)
+
+          expect(read_data).to eq "\x00\x01\x02\x03"
+          expect(read_data.length).to eq 4
+
+          `rm #{first_file['filename']}`
+        end
+      end
+
+      context "when writing to multiple files" do
+        it "writes data the to the correct file" do
+          File.open(first_file['filename'], "w") do |f|
+            f.write("\x00\x01\x02\x03")
+          end
+          File.open(other_file['filename'], "w") do |f|
+            f.write("\x04\x05\x06\x07\x08")
+          end
+
+          files = [DownloadableFile.new(first_file), DownloadableFile.new(other_file)]
+          info_dictionary = double("info_dictionary", files: [files].flatten)
+          reader = FileReader.new(info_dictionary)
+
+          byte_range = 3...6
+          read_data = reader.read(byte_range)
+
+          expect(read_data).to eq "\x03\x04\x05"
+          expect(read_data.length).to eq 3
+
+          `rm #{first_file['filename']}`
+          `rm #{other_file['filename']}`
+        end
+      end
+    end
+  end
+end
